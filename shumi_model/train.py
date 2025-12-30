87a8314ec9b85047d0fac770baacba7b58b417f1
SHENGYANG SHI <shengyangshi@mac.lan>
2025-12-29 22:34:59 -0800

    Improved the architecture by using attention model, tril, parallel inputs and outputs with one index shift, multi-head attention, multi-head output

--- shumi_model/train.py
+++ shumi_model/train.py
@@ -29,6 +29,12 @@ def getDevice() -> str:
 
 
 device = getDevice()
+head_num = 4
+input_num = 15
+embedding_num = 128
+block_size = 16
+batch_size = 32
+print(f"Using device: {device}")
 
 
 # Loads Shumi actions from the JSON file. Ordered by date and time in an ascending order.
@@ -96,28 +102,34 @@ def getShumiActions() -> list[ShumiAction]:
         return shumi_actions
 
 
-# Convert a ShumiAction to embedding tensor in shape [8].
+def one_hot(enum_val: int, num_classes: int):
+    if enum_val <= 0 or enum_val > num_classes:
+        return torch.zeros(num_classes).float()
+    return F.one_hot(torch.tensor(enum_val) - 1, num_classes=num_classes).float()
+
+
+# Convert a ShumiAction to embedding tensor in shape [15].
 # The features are:
-#   - Action Type (float)
-#   - Milk Type (float)
-#   - Milk Amount (float)
-#   - Daiper Type (float)
-#   - Sleep Duration in minutes (float)
-#   - Since Previous Action Duration in minutes (float)
-#   - Time Hour (float)
-#   - Time Minute (float)
+#   - Action Type (float, 3)
+#   - Milk Type (float, 3)
+#   - Milk Amount (float, 1)
+#   - Daiper Type (float, 4)
+#   - Sleep Duration in minutes (float, 1)
+#   - Since Previous Action Duration in minutes (float, 1)
+#   - Time Hour (float, 1)
+#   - Time Minute (float, 1)
 def getActionEmbedding(shumi_action: ShumiAction) -> torch.Tensor:
-    action_type_tensor = torch.tensor([shumi_action.action.value], dtype=torch.float32)
-    milk_type_tensor = torch.tensor(
-        [shumi_action.milk_type.value if shumi_action.milk_type is not None else 0],
-        dtype=torch.float32,
+    action_type_tensor = one_hot(shumi_action.action.value, len(Action))
+    milk_type_tensor = one_hot(
+        shumi_action.milk_type.value if shumi_action.milk_type is not None else 0,
+        len(MilkType),
+    )
+    daiper_type_tensor = one_hot(
+        shumi_action.daiper_type.value if shumi_action.daiper_type is not None else 0,
+        len(DaiperType),
     )
     milk_amount_tensor = torch.tensor(
         [shumi_action.milk_amount if shumi_action.milk_amount is not None else 0],
-        dtype=torch.float32,
-    )
-    daiper_type_tensor = torch.tensor(
-        [shumi_action.daiper_type.value if shumi_action.daiper_type is not None else 0],
         dtype=torch.float32,
     )
     sleep_duration_tensor = torch.tensor(
@@ -205,7 +217,9 @@ def getShumiAction(action_tensor: torch.Tensor) -> ShumiAction:
 
 # Gets a batch of action embeddings for training and validation, in shape of [#, block_size, feature_size].
 # Where # is the number of data, feature size is the size of each action embedding returned by getActionEmbedding().
-def getActionEmbeddings(block_size: int = 16) -> tuple[torch.Tensor, torch.Tensor]:
+def getActionEmbeddings(
+    block_size: int = block_size,
+) -> tuple[torch.Tensor, torch.Tensor]:
     actions = getShumiActions()
     actions_tensor = torch.stack([getActionEmbedding(action) for action in actions])
 
@@ -225,13 +239,27 @@ def getActionEmbeddings(block_size: int = 16) -> tuple[torch.Tensor, torch.Tenso
             for end_offset in end_offsets
         ]
     )
-    outputs = torch.stack([actions_tensor[end_offset] for end_offset in end_offsets])
+    outputs = torch.stack(
+        [
+            # If the preceding actions do not have enough length, pad with zeros at the beginning.
+            F.pad(
+                actions_tensor[max(1, end_offset - block_size) : end_offset + 1],
+                (
+                    0,
+                    0,
+                    0,
+                    block_size - (end_offset - max(0, end_offset - block_size - 1)),
+                ),
+            )
+            for end_offset in end_offsets
+        ]
+    )
     return inputs, outputs
 
 
 # Gets training and validation data.
 def getData(
-    block_size: int = 16,
+    block_size: int = block_size,
 ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
     x, y = getActionEmbeddings(block_size)
     n = int(len(x) * 0.8)
@@ -244,7 +272,7 @@ train_x, train_y, test_x, test_y = getData()
 
 # Gets a batch of data for training or validation in one run.
 def getBatchData(
-    split: str = "train", batch_size: int = 32
+    split: str = "train", batch_size: int = batch_size
 ) -> tuple[torch.Tensor, torch.Tensor]:
     if split == "train":
         x, y = train_x, train_y
@@ -254,8 +282,44 @@ def getBatchData(
     return x[indices], y[indices]
 
 
+class Head(nn.Module):
+    def __init__(self, head_size: int, embedding_num: int = embedding_num):
+        super().__init__()
+        self.key = nn.Linear(embedding_num, head_size, bias=False)
+        self.query = nn.Linear(embedding_num, head_size, bias=False)
+        self.value = nn.Linear(embedding_num, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        weights = q @ k.transpose(-2, -1) * (embedding_num**-0.5)
+        weights = weights.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        weights = F.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
+
+        return weights @ self.value(x)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads: int, head_size: int):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [Head(head_size, num_heads * head_size) for _ in range(num_heads)]
+        )
+        self.proj = nn.Linear(num_heads * head_size, embedding_num)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
 class FeedForwardNN(nn.Module):
-    def __init__(self, embedding_size: int = 128):
+    def __init__(self, embedding_size: int = embedding_num):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(embedding_size, 4 * embedding_size),
@@ -269,32 +333,63 @@ class FeedForwardNN(nn.Module):
         return self.net(x)
 
 
-class ShumiPatternModel(nn.Module):
+class Block(nn.Module):
     def __init__(
-        self, input_size: int = 8, embedding_size: int = 128, block_size: int = 16
+        self,
+        head_num: int = head_num,
+        embedding_size: int = embedding_num,
     ):
         super().__init__()
-        # self.input_embedding_table = nn.Embedding(input_size, embedding_size)
-        # self.positional_embedding_table = nn.Embedding(block_size, embedding_size)
-        self.feed_forward_nn = nn.Sequential(
-            nn.Linear(input_size, 4 * embedding_size),
-            nn.ReLU(),
-            nn.Linear(4 * embedding_size, embedding_size),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-        )
-        self.output = nn.Linear(embedding_size, input_size)
+        self.multi_head = MultiHeadAttention(head_num, embedding_size // head_num)
+        self.ffwd = FeedForwardNN(embedding_size)
+        self.ln1 = nn.LayerNorm(embedding_size)
+        self.ln2 = nn.LayerNorm(embedding_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.feed_forward_nn(x)
-        x = self.output(x)
-        return x.mean(dim=1)
+        x = x + self.multi_head(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class ShumiPatternModel(nn.Module):
+    def __init__(
+        self,
+        input_size: int = input_num,
+        embedding_size: int = embedding_num,
+    ):
+        super().__init__()
+        self.proj = nn.Linear(input_size, embedding_size)
+        self.ln1 = nn.LayerNorm(embedding_size)
+        self.blocks = nn.Sequential(
+            Block(head_num, embedding_size),
+            Block(head_num, embedding_size),
+            Block(head_num, embedding_size),
+        )
+        self.action_type_head = self.head(embedding_size, 3)
+
+    def forward(self, x: torch.Tensor) -> dict[str, torch.Tensor]:
+        embedding = self.proj(x)
+        embedding = self.blocks(embedding)
+        embedding = self.ln1(embedding)
+        action_outputs = self.action_type_head(embedding)
+
+        return {
+            "action_type": action_outputs,
+        }
+
+    def head(self, input_dim: int, output_dim: int) -> nn.Sequential:
+        return nn.Sequential(
+            nn.Linear(input_dim, round(input_dim / 4)),
+            nn.ReLU(),
+            nn.Linear(round(input_dim / 4), output_dim),
+        )
 
 
 model = ShumiPatternModel()
 model = model.to(device)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-loss_fn = nn.MSELoss()
+mse_loss = nn.MSELoss()
+cross_entropy_loss = nn.CrossEntropyLoss()
 
 
 @torch.no_grad()
@@ -306,38 +401,55 @@ def estimate_loss() -> dict[str, float]:
     for split in ["train", "val"]:
         losses = torch.zeros(10)
         for k in range(10):
-            xb, yb = getBatchData(split, batch_size=32)
+            xb, yb = getBatchData(split, batch_size=batch_size)
             xb = xb.to(device)
             yb = yb.to(device)
             outputs = model(xb)
-            loss = loss_fn(outputs, yb)
-            losses[k] = loss.item()
+            action_loss = cross_entropy_loss(
+                outputs["action_type"].view(-1, 3),
+                yb[:, :, 0:3].argmax(dim=-1).view(-1),
+            )
+            losses[k] += action_loss.item()
         out[split] = losses.mean().item()
     model.train()
     print(f"Step {iter}: train loss {out['train']:.4f}, val loss {out['val']:.4f}")
     return out
 
 
-for iter in range(10000):
-    xb, yb = getBatchData("train", batch_size=32)
+train_time_start = datetime.datetime.now()
+train_time_prev = train_time_start
+
+for iter in range(5000):
+    xb, yb = getBatchData("train", batch_size=batch_size)
     xb = xb.to(device)
     yb = yb.to(device)
     optimizer.zero_grad(set_to_none=True)
     outputs = model(xb)
-    loss = loss_fn(outputs, yb)
+    loss = cross_entropy_loss(
+        outputs["action_type"].view(-1, 3), yb[:, :, 0:3].argmax(dim=-1).view(-1)
+    )
     loss.backward()
     optimizer.step()
 
     if iter % 1000 == 0:
         estimate_loss()
+        train_time_now = datetime.datetime.now()
+        elapsed = train_time_now - train_time_prev
+        total_elapsed = train_time_now - train_time_start
+        train_time_prev = train_time_now
+        print(
+            f"Elapsed time for last 1000 iters: {elapsed}, total elapsed time: {total_elapsed}"
+        )
 
 with torch.no_grad():
-    actions = getShumiActions()[-16:]
+    actions = getShumiActions()[-block_size:]
     last_actions = (
         torch.stack([getActionEmbedding(action) for action in actions])
-        .expand(32, 16, 8)
+        .unsqueeze(0)
         .to(device)
     )
     output = model(last_actions)
-    predicted_next_action = getShumiAction(output.mean(dim=0))
-    print(f"Predicted next action: {predicted_next_action}")
+    action_probs = F.softmax(output["action_type"][:, -1, :], dim=-1)
+    action_type_val = torch.argmax(action_probs).item() + 1
+    action = Action(action_type_val)
+    print(f"Predicted next action: {action}, probabilities: {action_probs}")
